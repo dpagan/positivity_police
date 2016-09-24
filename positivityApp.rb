a7c22ae55a187f45b7790d93a02c5f4a21d9c011
Daniel Pagan <Daniel Pagan>
2016-09-24 14:29:24 -0400

    sentiment basics

--- positivityApp.rb
+++ positivityApp.rb
@@ -1,3 +1,4 @@
+require 'unirest'
 require 'twitter'
 require 'dotenv'
 
@@ -15,3 +16,16 @@ end
 client.search("from:realdonaldtrump", :result_type => "recent").take(1).collect do |tweet|
     p "#{tweet.user.screen_name}: #{tweet.text}"
 end
+
+# These code snippets use an open-source library.
+response = Unirest.post "https://twinword-sentiment-analysis.p.mashape.com/analyze/",
+  headers:{
+    "X-Mashape-Key" => "XnKlAmUXLjmshcnfCeF7YfNDFcwAp17eaQijsngkJBFBBXeGPS",
+    "Content-Type" => "application/x-www-form-urlencoded",
+    "Accept" => "application/json"
+  },
+  parameters:{
+    "text" => "great value in its price range!"
+  }
+
+  p response
