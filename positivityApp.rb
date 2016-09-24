86f2f120d307dc58c4e4e628d1953f60998e9170
Daniel Pagan <Daniel Pagan>
2016-09-24 13:45:27 -0400

    Trump tweeting

--- positivityApp.rb
+++ positivityApp.rb
@@ -12,6 +12,6 @@ client = Twitter::REST::Client.new do |config|
     config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
 end
 
-client.search("hate", :result_type => "recent").take(1).collect do |tweet|
+client.search("from:realdonaldtrump", :result_type => "recent").take(1).collect do |tweet|
     p "#{tweet.user.screen_name}: #{tweet.text}"
 end
